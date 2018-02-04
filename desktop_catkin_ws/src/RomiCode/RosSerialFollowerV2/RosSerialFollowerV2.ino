957fcc7fca67070f8480c69bdf24828720ed23ea
ShaneRozenLevy <35812036+ShaneRozenLevy@users.noreply.github.com>
2018-02-04 18:20:28 -0500

    Update RosSerialFollowerV2.ino

--- desktop_catkin_ws/src/RomiCode/RosSerialFollowerV2/RosSerialFollowerV2.ino
+++ desktop_catkin_ws/src/RomiCode/RosSerialFollowerV2/RosSerialFollowerV2.ino
@@ -78,18 +78,21 @@ void loop()
   int maxSpeed=6;
   float r=.13278/2.0;
   
+  //read encoder posistions
   long temp_posA=-1*encoders.getCountsLeft();
   long temp_posB=-1*encoders.getCountsRight();
   long tNew=millis();
   double dt=tNew-tOld;
+  //factor is based on ms to s, diamater of wheels and mm to m
   double factor=1000.0/1440.0*220.0/1000.0;
 
+  //see if the encoders had an issue
   if(!encoders.checkErrorLeft())
   {
       if(abs((temp_posA-last_posA)/(dt))<maxSpeed)
       {
         spdA=(temp_posA-last_posA)/(dt);// multiplied by 1000 to change into units of s, divide by 1440 to turn into revs/s, times 220 for mm/s
-        spdA=factor*constrain(spdA,-1*maxSpeed,maxSpeed);
+        spdA=factor*constrain(spdA,-1*maxSpeed,maxSpeed);//prevent huge speeds when an error occurs
       }
 
   }
@@ -99,7 +102,7 @@ void loop()
       if(abs((temp_posB-last_posB)/(dt))<maxSpeed)
       {
         spdB=(temp_posB-last_posB)/(dt);
-        spdB=factor*constrain(spdB,-1*maxSpeed,maxSpeed);
+        spdB=factor*constrain(spdB,-1*maxSpeed,maxSpeed);//prevent huge speeds when errors occur
       }
   }
 
@@ -109,19 +112,19 @@ void loop()
   rpm.publish(&rpm_msg);
 
   imu.read();
-  if(abs(spdA-spdB) < 0.02)
+  if(abs(spdA-spdB) < 0.02)//if the wheels are going close to the same speed rezero the gyro
   {
     gyro_offset=imu.g.z;
   }
   gyro_msg.data=(imu.g.z-gyro_offset)*8.75/1000*3.14159/180;
   gyro.publish(&gyro_msg);
 
-  errorA=xspd_ref+thspd_ref*r-spdA+errorA;
+  errorA=xspd_ref+thspd_ref*r-spdA+errorA; calculate the cumulative error
   errorB=xspd_ref-thspd_ref*r-spdB+errorB;
 
   if ( errorA > 0)
     {
-      mA=(int)constrain(ki*errorA+offset,-300,300);
+      mA=(int)constrain(ki*errorA+offset,-300,300); //the offset is added in to deal with friction in the motor
     }
   else if (errorA<0)
     {
